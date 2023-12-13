c029b027062c2f713443b9176f5941fdfabd71de
Daniel Cornflakes <daniel.jochheim.uni@gmail.com>
2023-12-13 16:20:40 +0100

    even more cleanup, still works

--- bonk_code.lua
+++ bonk_code.lua
@@ -32,40 +32,15 @@ thief = {
     animSpeed = 12, -- Adjust the speed to control the animation
 }
 
-
 bones = {
     max = 10,
     spawn_interval = 100, --this number refers to frames that have happened (adjust as needed)
 }
 
 
-
-
-
-
-
-
-
 -->8
--- Function declarations
+-- bonk functions
 
--- Loop to ensure the thief doesn't move straight right, up, or down
-function ensure_thief_diagonal()
-    while thief.direction.x == 0 or thief.direction.y == 0 do
-        thief.direction = {x = flr(rnd(3)) - 1, y = flr(rnd(3)) - 1}
-    end
-end
-
--- Function for drawing thief {#c0c}
-function drawThief()
-    if thief.flip then
-        spr(thief.sprites[thief.currentFrame][1], thief.pos.x, thief.pos.y, 1, 1, true, false)
-        spr(thief.sprites[thief.currentFrame][2], thief.pos.x, thief.pos.y + 8, 1, 1, true, false)
-    else
-        spr(thief.sprites[thief.currentFrame][1], thief.pos.x, thief.pos.y)
-        spr(thief.sprites[thief.currentFrame][2], thief.pos.x, thief.pos.y + 8)
-    end
-end
 
 -- Function for drawing bonk {#c81}
 function drawBonk()
@@ -74,50 +49,6 @@ function drawBonk()
     else
         spr(bonk.sprites[bonk.currentFrame], bonk.pos.x, bonk.pos.y)
     end
-end
-
--- Spawn a new bone into the bone table
-function spawnBone()
-    -- Check if there are fewer than the maximum allowed bones
-    if #bones < bones.max then
-        -- Spawn a new bone at a random location and set it to active = true
-        bones[#bones + 1] = {
-            pos = {x = rnd(120), y = rnd(120)}, -- random spawn position
-            --x = rnd(120),
-            --y = rnd(120),
-            width = 8,
-            height = 4,
-            active = true,
-            collected = false,
-            sprites = {36, 52},
-            currentFrame = 1,
-            anim_counter = 0,
-            anim_speed = 20, --every 20 frames the animation updates to next frame
-            }
-    end
-end
-
--- Draw Bones with animation frames {#fff}
-function draw_bones()
-    for i = 1, #bones do
-        if bones[i] and bones[i].active then
-            spr(bones[i].sprites[bones[i].currentFrame], bones[i].pos.x, bones[i].pos.y)
-        end
-    end
-end
-
---Print help text on screen
-function timer()
-    print(time(),50,10,7)
-end
-
--- collision function {#0f0}
-function checkcollision(obj1, obj2)
-    if (obj1.pos.x > obj2.pos.x + obj2.width) then return false end
-    if (obj1.pos.y > obj2.pos.y + obj2.height) then return false end
-    if (obj1.pos.x + obj1.width < obj2.pos.x) then return false end
-    if (obj1.pos.y + obj1.height < obj2.pos.y) then return false end
-    return true
 end
 
 -- movement and animation updates {#c81}
@@ -169,6 +100,9 @@ function bonk_animation()
     end
 end
 
+-->8
+-- thief functions
+
 -- Move the thief randomly {#c0c}
 function thief_movement()
     -- Move thief
@@ -191,7 +125,12 @@ function thief_movement()
     end
 end
 
-ensure_thief_diagonal()
+-- Loop to ensure the thief doesn't move straight right, up, or down
+function ensure_thief_diagonal()
+    while thief.direction.x == 0 or thief.direction.y == 0 do
+        thief.direction = {x = flr(rnd(3)) - 1, y = flr(rnd(3)) - 1}
+    end
+end
 
 -- Update thief animation
 function thief_animation()
@@ -206,6 +145,93 @@ function thief_animation()
                 thief.currentFrame = 1
             end
         end
+end
+
+-- Function for drawing thief {#c0c}
+function drawThief()
+    if thief.flip then
+        spr(thief.sprites[thief.currentFrame][1], thief.pos.x, thief.pos.y, 1, 1, true, false)
+        spr(thief.sprites[thief.currentFrame][2], thief.pos.x, thief.pos.y + 8, 1, 1, true, false)
+    else
+        spr(thief.sprites[thief.currentFrame][1], thief.pos.x, thief.pos.y)
+        spr(thief.sprites[thief.currentFrame][2], thief.pos.x, thief.pos.y + 8)
+    end
+end
+
+
+
+--Print help text on screen
+function timer()
+    print(time(),50,10,7)
+end
+
+
+-->8
+-- bone functions
+
+-- Check if it's time to spawn a new bone and do it{#fff}
+
+-- How to spawn a new bone into the bone table
+function spawnBone()
+    -- Check if there are fewer than the maximum allowed bones
+    if #bones < bones.max then
+        -- Spawn a new bone at a random location and set it to active = true
+        bones[#bones + 1] = {
+            pos = {x = rnd(120), y = rnd(120)}, -- random spawn position
+            --x = rnd(120),
+            --y = rnd(120),
+            width = 8,
+            height = 4,
+            active = true,
+            collected = false,
+            sprites = {36, 52},
+            currentFrame = 1,
+            anim_counter = 0,
+            anim_speed = 20, --every 20 frames the animation updates to next frame
+            }
+    end
+end
+
+-- Spawn a bone after time elapses if under 10 bones
+function spawn_new_bone()
+    if elapsedTime >= bones.spawn_interval then
+        elapsedTime = 0
+        spawnBone()
+    end
+end
+
+-- Draw Bones with animation frames {#fff}
+function draw_bones()
+    for i = 1, #bones do
+        if bones[i] and bones[i].active then
+            spr(bones[i].sprites[bones[i].currentFrame], bones[i].pos.x, bones[i].pos.y)
+        end
+    end
+end
+
+-- Update bone animation {#fff}
+function bone_animation()
+    for i = 1, #bones do
+        if bones[i] and bones[i].active then
+            bones[i].anim_counter = bones[i].anim_counter + 1
+            if bones[i].anim_counter % bones[i].anim_speed == 0 then
+                bones[i].currentFrame = 3 - bones[i].currentFrame -- Toggle between 1 and 2 frames
+            end
+        end
+    end
+end
+
+
+-->8
+-- collision functions
+
+-- main collision function {#0f0}
+function checkcollision(obj1, obj2)
+    if (obj1.pos.x > obj2.pos.x + obj2.width) then return false end
+    if (obj1.pos.y > obj2.pos.y + obj2.height) then return false end
+    if (obj1.pos.x + obj1.width < obj2.pos.x) then return false end
+    if (obj1.pos.y + obj1.height < obj2.pos.y) then return false end
+    return true
 end
 
 --Check collision between thief and bonk {#c0c}
@@ -229,46 +255,29 @@ end
 
 
 
+-->8
+-- Other functions
+
 -- Increase overall timer {#fff}
 function game_timer()
     elapsedTime = elapsedTime + 1  -- Increment elapsed time by 1 frame
 end
 
--- Check if it's time to spawn a new bone and do it{#fff}
-function spawn_new_bone()
-    if elapsedTime >= bones.spawn_interval then
-        elapsedTime = 0
-        spawnBone()
-    end
-end
 
--- Update bone animation {#fff}
-function bone_animation()
-    for i = 1, #bones do
-        if bones[i] and bones[i].active then
-            bones[i].anim_counter = bones[i].anim_counter + 1
-            if bones[i].anim_counter % bones[i].anim_speed == 0 then
-                bones[i].currentFrame = 3 - bones[i].currentFrame -- Toggle between 1 and 2 frames
-            end
-        end
-    end
-end
 
 -->8
 --update
 
--- update function {#f00,1}
 function _update()
-
-game_timer()
-bonk_movement()
-thief_movement()
-thief_animation()
-collision_bonk_thief()
-collision_bonk_bone()
-spawn_new_bone()
-bone_animation()
-
+    game_timer()
+    bonk_movement()
+    thief_movement()
+    ensure_thief_diagonal()
+    thief_animation()
+    collision_bonk_thief()
+    collision_bonk_bone()
+    spawn_new_bone()
+    bone_animation()
 end
 
 
@@ -276,26 +285,10 @@ end
 -->8
 --draw
 
--- Draw function {#f00}
 function _draw()
-
-
--- Screen background color
     cls(0)
-
---timer on screen
     timer()
-
-
-
-draw_bones()
-
--- Draw Bonk {#c81}
+    draw_bones()
     drawBonk()
-
-
--- Draw Thief {#c0c}
     drawThief()
-
-
 end
